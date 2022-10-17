15b38e9718c26a6d6d449c490a0af14d770ceaa1
Onkar <onkar.kotasthane@gmail.com>
2022-10-17 21:59:01 +0530

    Source code refactored.

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,47 +4,53 @@ import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.variables.IntVar;
-
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Choco Solver LP Sample demo application
+ * */
 public class Application {
+    Logger logger = Logger.getLogger(this.getClass().toString());
 
     public static void main(String[] args) {
         new Application().begin();
     }
     private void begin() {
-        System.out.println("begin");
 
+        // define LP model
         Model model = new Model("test_model");
 
-        // variable-wise possible values
+        // variable space
         IntVar v1 = model.intVar("v1", new int[]{0, 100, 200, 300, 4500, 500});
         IntVar v2 = model.intVar("v1", new int[]{0, 100, 200, 3000, 400, 500});
         IntVar v3 = model.intVar("v1", new int[]{0, 400, 700, 300});
 
-
-        // consolidate the variables
+        // consolidated variables array
         IntVar[] vars = new IntVar[]{v1, v2, v3};
 
-        model.sum(vars, "=", 1000).post();
+        /*  defining operation with condition.
+            condition: sum of all three variables (i.e. v1, v2 and v3) must be equal to constant (i.e.1000)
+         */
+        final int constant = 1000;
+        model.sum(vars, "=", constant).post();
 
+        // execute the expression
         Solver solver = model.getSolver();
 
+        // get all feasible solutions
         List<Solution> solutions = solver.findAllSolutions();
+        int totalSolutionsCount = solutions.size();
+        logger.log(Level.INFO, "total solutions {0} {1}", new Object[]{totalSolutionsCount, 12});
 
-        System.out.println("total solutions " + solutions.size());
-
+        // iterating over solutions for doing more operations
         solutions.stream().forEach(solution -> {
-
-            int v1_solutionValue = solution.getIntVal(v1);
-            int v2_solutionValue = solution.getIntVal(v2);
-            int v3_solutionValue = solution.getIntVal(v3);
-
-            int sum_ofSolutionValues = v1_solutionValue + v2_solutionValue + v3_solutionValue;
-
-            System.out.println(v1_solutionValue + "-" + v2_solutionValue + "-" + v3_solutionValue + " = " + sum_ofSolutionValues);
-
+            int v1SolutionValue = solution.getIntVal(v1);
+            int v2SolutionValue = solution.getIntVal(v2);
+            int v3SolutionValue = solution.getIntVal(v3);
+            int sumOfSolutionValues = v1SolutionValue + v2SolutionValue + v3SolutionValue;
+            logger.log(Level.INFO, "v1:{0}, v2:{1}, v2:{2} = {3}", new Object[]{v1SolutionValue,v2SolutionValue,v3SolutionValue,sumOfSolutionValues});
         });
-
     }
 }
