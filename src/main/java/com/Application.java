dccf97603a9443e0dd5d9dcc13f513f703cf96de
Onkar <onkar.kotasthane@gmail.com>
2022-11-11 14:06:59 +0530

    added an application constructor

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -18,6 +18,7 @@ public class Application {
     public static void main(String[] args) {
         new Application().begin();
     }
+    public Application() {} // default constructor
     private void begin() {
 
         // define LP model
