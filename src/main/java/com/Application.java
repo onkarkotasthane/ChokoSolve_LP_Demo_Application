609f95d8d9ec5bb789125e54c417ded5d29bfeca
Onkar Kotasthane <onkarkotasthane2009@gmail.com>
2022-11-11 14:18:25 +0530

    T#4 (#1)
    * added line
    
    * added an application constructor
    
    * Revert "added line"
    
    This reverts commit f60a1a9d
    
    Co-authored-by: Onkar <onkar.kotasthane@gmail.com>

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -18,7 +18,7 @@ public class Application {
     public static void main(String[] args) {
         new Application().begin();
     }
-
+    public Application() {} // default constructor
     private void begin() {
 
         // define LP model
